d223e6cda389837aaf60d0f52f4b1df51831c9d2
kujirahand <web@kujirahand.com>
2023-07-19 13:13:12 +0900

    update build_release.sh

--- build_relaese.sh
+++ build_relaese.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 TARGET=$SCRIPT_DIR/picosakura-pack
+
+# cargo
 cargo build --release
 cp target/release/picosakura $TARGET/
+
+mkdir -p $TARGET
+
+# zip
+cd $SCRIPT_DIR
+rm picosakura-pack.zip
+zip picosakura-pack.zip -r picosakura-pack -x "*.DS_Store" "*__MACOSX*"
 echo "ok"
 
